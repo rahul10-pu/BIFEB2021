66e9d9ed29b5c3231a8220589dbdc4f37d34af79
Rahul Kumar <rahul10.pu@gmail.com>
2021-03-07 20:25:05 +0530

    hellowworld code, variables, print

---
+++ helloworld.js
@@ -0,0 +1,18 @@
+console.log("Hellooooooooooooooooooooooooo World");
+console.log("Hellooooooooooooooooooooooooo World");
+console.log("Hellooooooooooooooooooooooooo World");
+console.log("Hellooooooooooooooooooooooooo World");
+console.log("Hellooooooooooooooooooooooooo World");
+console.log("Hellooooooooooooooooooooooooo World");
+console.log("Hellooooooooooooooooooooooooo World");
+console.log("Hellooooooooooooooooooooooooo World");
+console.log("Hellooooooooooooooooooooooooo World");
+console.log("Hellooooooooooooooooooooooooo World");
+console.log("Hellooooooooooooooooooooooooo World");
+var mydata="World";
+
+console.log(mydata);
+var first=19;
+var second=10;
+var result=first+second;
+console.log(result);
