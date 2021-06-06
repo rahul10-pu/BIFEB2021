a314856444f1abd42fe04fb3be004eb7253b6580
Rahul Kumar <rahul.1.kumar@seagate.com>
2021-06-06 18:43:00 +0530

    all middleware to api

--- middleware/authJWT.js
+++ middleware/authJWT.js
@@ -4,18 +4,21 @@
 //isModeratorOrAdmin
 
 import jwt from 'jsonwebtoken'
-import config from '../config/auth.config.js'
+import  {secret}  from '../config/auth.config.js'
 import db from '../models/index.js';
 const User = db.users
 //"x-access-token": "jwt token"
-verifyToken=(req, res, next)=>{
-    let token = req.header["x-access-token"];
+const verifyToken=(req, res, next)=>{
+    console.log(req.headers)
+    console.log(req.headers["x-access-token"])
+    let token = req.headers["x-access-token"];
+
     if(!token){
         return res.status(403).send({
             message:"No token provided, Access forbidden"
         })
     }
-    jwt.verify(token, config.secret, (err, decode)=>{
+    jwt.verify(token, secret, (err, decode)=>{
         if(err){
             return res.status(401).send({
                 message: "Unauthorized!"
@@ -26,7 +29,7 @@ verifyToken=(req, res, next)=>{
     })
 }
 
-isAdmin = (req, res, next)=>{
+const isAdmin = (req, res, next)=>{
     User.findByPk(req.userId)
         .then(
             (user)=>{
@@ -48,9 +51,48 @@ isAdmin = (req, res, next)=>{
             }
         )
 }
-
-const authJWT = {
-    verifyToken:verifyToken,
-    isAdmin:isAdmin
-};
-export default authJWT
+const isModerator = (req, res, next) => {
+    User.findByPk(req.userId).then(user => {
+      user.getRoles().then(roles => {
+        for (let i = 0; i < roles.length; i++) {
+          if (roles[i].name === "moderator") {
+            next();
+            return;
+          }
+        }
+  
+        res.status(403).send({
+          message: "Require Moderator Role!"
+        });
+      });
+    });
+  };
+  
+  const isModeratorOrAdmin = (req, res, next) => {
+    User.findByPk(req.userId).then(user => {
+      user.getRoles().then(roles => {
+        for (let i = 0; i < roles.length; i++) {
+          if (roles[i].name === "moderator") {
+            next();
+            return;
+          }
+  
+          if (roles[i].name === "admin") {
+            next();
+            return;
+          }
+        }
+  
+        res.status(403).send({
+          message: "Require Moderator or Admin Role!"
+        });
+      });
+    });
+  };
+  
+  export const authJwt = {
+    verifyToken: verifyToken,
+    isAdmin: isAdmin,
+    isModerator: isModerator,
+    isModeratorOrAdmin: isModeratorOrAdmin
+  };
--- routes/tutorials.js
+++ routes/tutorials.js
@@ -1,11 +1,12 @@
 import express from 'express';
+import {authJwt} from '../middleware/authJWT.js'
 import {getAllTutorialsByTitle,createTutorial,deleteTutorialByID,deleteTutorials,updateTutorialByID} from '../controllers/tutorial.js'
 
 const router=express.Router();
 
 router.get("/",getAllTutorialsByTitle)
-router.post('/',createTutorial)
-router.delete('/',deleteTutorials)
-router.delete("/:id", deleteTutorialByID)
-router.put("/:id",updateTutorialByID)
+router.post('/',[authJwt.verifyToken, authJwt.isAdmin],createTutorial)
+router.delete('/',[authJwt.verifyToken, authJwt.isModerator],deleteTutorials)
+router.delete("/:id", [authJwt.verifyToken],deleteTutorialByID)
+router.put("/:id",[authJwt.verifyToken, authJwt.isModerator],updateTutorialByID)
 export default router //
